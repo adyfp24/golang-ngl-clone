10c471095ec1e9e7a3aa058b2c99bb020904330d
adyfp24 <adyfp24@gmail.com>
2024-06-26 14:09:01 +0700

    feat: ui for send message

--- internal/models/chat.go
+++ internal/models/chat.go
@@ -8,6 +8,7 @@ type Chat struct{
 	gorm.Model
 	Question string `gorm:"not null"`
 	Answer string
+	isOpen bool `gorm:"default:false"`
 	UserID uint 
 	User User `gorm:"constraint:OnDelete:CASCADE,OnUpdate:CASCADE"`
 }
